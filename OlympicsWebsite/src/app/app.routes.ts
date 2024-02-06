d3f70e49ec49d1b403dae8e306ef102fd6d5e8ee
Teodor Kovachev <tedox.k@gmail.com>
2024-02-07 00:56:39 +0200

    'navigation_and_events'

--- OlympicsWebsite/src/app/app.routes.ts
+++ OlympicsWebsite/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SponsorsPageComponent } from './sponsors-page/sponsors-page.component';
+import { EventsPageComponent } from './events-page/events-page.component';
 
 export const routes: Routes = [
     {path:'',component:LandingPageComponent},
     {path:'sponsors',component:SponsorsPageComponent},
+    {path:'events/:event',component:EventsPageComponent}
 ];
