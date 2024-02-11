1c9caa724a72b050dcaeb733f6fa03995280a276
g-slavov <georgisslavov@gmail.com>
2024-02-12 00:38:20 +0200

    feat: finish sports page

--- OlympicsWebsite/src/app/data/sports.ts
+++ OlympicsWebsite/src/app/data/sports.ts
@@ -42,10 +42,81 @@ export const ddaySports = [
         RulesUrl: 'https://drive.google.com/file/d/1qxZWE_bzLeo2EYLThV_uBFa2h6whyZFV/view'
     },
     {
-        Title: 'Long Jump',
+        Title: 'Marathon',
         Icon: '',
-        Headline: "HOW FAR CAN YOU GO?",
-        Description: "Standing long jump is a track and field event in which the athlete stands at a line, and then jumps as further from this point as possible.",
-        RulesUrl: 'https://drive.google.com/file/d/1qxZWE_bzLeo2EYLThV_uBFa2h6whyZFV/view'
-    }
+        Headline: "SERIOUSLY. HOW FAR CAN YOU GO?",
+        Description: "This is a 4.5 km distance running event, organized on the day before the AUBG Olympic Games. Participants compete in female and male divisions. Here the ones who win are not the fastest ones, but the ones who endure. Stretch well because good preparation is a sign of potential.",
+        RulesUrl: 'https://drive.google.com/file/d/1AC_mF6XeDyxQaLj7PeLwXN6uhJcoEJzU/view'
+    },
+    {
+        Title: 'Powerlifting',
+        Icon: '',
+        Headline: "BRO, DO YOU EVEN LIFT?",
+        Description: "Challenge yourself in this form of competitive weightlifting, where contestants attempt three types of exercises in a set sequence. The result is calculated according to the Wilks calculator, which takes into consideration the contestant's weight and the kilograms lifted.",
+        RulesUrl: 'https://drive.google.com/file/d/1tnYsSpupDK6PKrB023LooH4BQAI2kzDa/view'
+    },
+
+    {
+        Title: 'Relay 4x100 Meters',
+        Icon: '',
+        Headline: "IT'S DASH BUT LONGER!",
+        Description: "Relay 4x100 meters gets teams of 4 people to compete over a track with length of 400 meters, having each team member run 100 meters of the track. The players pass a baton to each other within a 20m changeover box. The team that first reaches the finish line without dropping the baton or failing to pass it within the 20m changeover box wins.",
+        RulesUrl: 'https://drive.google.com/file/d/1tnYsSpupDK6PKrB023LooH4BQAI2kzDa/view'
+    },
+    {
+        Title: 'Skills Challenge',
+        Icon: '',
+        Headline: "GOT SKILLS?",
+        Description: "All participants will compete under the same rules. There are men and women brackets. All completion times are recorded. The three best times will be awarded with either Gold, Silver or Bronze medals respectively. The skills track changes every year.",
+        RulesUrl: 'https://drive.google.com/file/d/1tnYsSpupDK6PKrB023LooH4BQAI2kzDa/view'
+    },
+    {
+        Title: 'Soccer',
+        Icon: '',
+        Headline: "FEEL THE GAME, EMBRACE THE MAGIC.",
+        Description: "Be part of the most popular sport on the planet. Two teams of 6 players. Each team has 5 field players and one goalkeeper. The object of the game is to score by getting the ball into the opponent's net. The challenge follows the basic rules of traditional soccer in a relaxed form similar to futsal.",
+        RulesUrl: 'https://drive.google.com/file/d/1tnYsSpupDK6PKrB023LooH4BQAI2kzDa/view'
+    },
+    {
+        Title: 'Spikeball',
+        Icon: '',
+        Headline: "SPIKE THE BALL. IT'S THAT SIMPLE.",
+        Description: "The sport of roundnet, a.k.a. \"that yellow trampoline game\" a.k.a. \"if volleyball and foursquare had a baby\" a.k.a. \"enough with the wit I'd like to read the rules,\" is played 2 vs 2. A player starts a point by serving the ball down onto the Spikeball net so it ricochets up at his opponents.",
+        RulesUrl: 'https://drive.google.com/file/d/1tnYsSpupDK6PKrB023LooH4BQAI2kzDa/view'
+    },
+    {
+        Title: 'Swimming',
+        Icon: '',
+        Headline: "IT'S JUST YOU AND THE WATER.",
+        Description: "Dive into the deep and prepare yourself for King of the Sprint individual 50m freestyle. Make time run in your favor while your muscles are bursting with energy. The swimming contest takes place at Swimming Club Pirin. Don't forget your swimming cap.",
+        RulesUrl: 'https://drive.google.com/file/d/1tnYsSpupDK6PKrB023LooH4BQAI2kzDa/view'
+    },
+    {
+        Title: 'Table Tennis',
+        Icon: '',
+        Headline: "IT'S TENNIS ON A TABLE. REALLY.",
+        Description: "Table tennis is a dynamic sport played on a rectangular table. There are singles and doubles brackets separated in men and women divisions. Similar to tennis, opponents need to pass a lightweight ball across the table using a small round paddle. Remember, it's always better to outwit your opponents than to outhit them.",
+        RulesUrl: 'https://drive.google.com/file/d/1tnYsSpupDK6PKrB023LooH4BQAI2kzDa/view'
+    },
+    {
+        Title: 'Tennis',
+        Icon: '',
+        Headline: "GRACE, SPEED, STRATEGY.",
+        Description: "These are all qualities required by one of the most sophisticated sports - tennis. Each player uses a racket that is strung with cord to strike a rubber ball. The object of the game is to pass the ball into the opponent's court in such a way that he/she cannot make a valid return.",
+        RulesUrl: 'https://drive.google.com/file/d/1tnYsSpupDK6PKrB023LooH4BQAI2kzDa/view'
+    },
+    {
+        Title: 'Tug of War',
+        Icon: '',
+        Headline: "FOR ROPE-HANDLING AND STRENGTH GODS ONLY.",
+        Description: "As the sport with the most spectators on our event, it is also the most competitive one. A contest in which two teams pull at opposite ends of a rope until one drags the other over a central line. There are 5 people per team and their joint efforts create the emotion of the game.",
+        RulesUrl: 'https://drive.google.com/file/d/1tnYsSpupDK6PKrB023LooH4BQAI2kzDa/view'
+    },
+    {
+        Title: 'Volleyball',
+        Icon: '',
+        Headline: "GOOD PASS, GOOD SET, GOOD BYE.",
+        Description: "Volleyball is one of the most popular sports in the world. Two teams of six compete against each other. The idea of the game is to get the ball over the net and into the other team's field. It has been officially included as an Olympics sport in the 1964 Summer Olympics.",
+        RulesUrl: 'https://drive.google.com/file/d/1tnYsSpupDK6PKrB023LooH4BQAI2kzDa/view'
+    },
 ]
--- OlympicsWebsite/src/app/sports-page/sports-page.component.ts
+++ OlympicsWebsite/src/app/sports-page/sports-page.component.ts
@@ -3,11 +3,12 @@ import { SportWidgetComponent } from '../components/sport-widget/sport-widget.co
 import { Sport } from '../models/sport';
 import { ddaySports } from '../data/sports';
 import { CommonModule } from '@angular/common';
+import { NavigationComponent } from '../navigation/navigation.component';
 
 @Component({
   selector: 'app-sports-page',
   standalone: true,
-  imports: [SportWidgetComponent, CommonModule],
+  imports: [SportWidgetComponent, CommonModule, NavigationComponent],
   templateUrl: './sports-page.component.html',
   styleUrl: './sports-page.component.css'
 })
