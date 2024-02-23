46345ec626698ddb26d62c0d09f71a7bbf71ed9f
Teodor Kovachev <tedox.k@gmail.com>
2024-02-23 16:42:45 +0200

    emails work, updated landing page timer

--- OlympicsWebsite/src/app/contact-us-page/contact-us-page.component.ts
+++ OlympicsWebsite/src/app/contact-us-page/contact-us-page.component.ts
@@ -1,27 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, Input, input, ViewEncapsulation,ViewChild } from '@angular/core';
+import { FormsModule, FormBuilder,ReactiveFormsModule } from '@angular/forms';
 import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+import { CommonModule } from '@angular/common';
+import { NavigationComponent } from '../navigation/navigation.component';
 
 @Component({
   selector: 'app-contact-us-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule,CommonModule,NavigationComponent],
   templateUrl: './contact-us-page.component.html',
-  styleUrl: './contact-us-page.component.css'
+  styleUrl: './contact-us-page.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class ContactUsPageComponent {
-  public sendEmail() {
-    const templateParams = {
-      name: 'James',
-      notes: 'Check this out!',
-    };
+  @ViewChild(NavigationComponent) nav?: NavigationComponent;
+  constructor(private formBuilder: FormBuilder){}  
+  emailForm = this.formBuilder.group({
+    name: '',
+    email: '',
+    text:''
+  });
+
+  nameError='';
+  emailError='';
+  textError='';
+
+  public validate(){
+    this.nameError='';
+    this.emailError='';
+    this.textError=''
+    console.log('^\S+@\S+\.\S+$');
+    let regex = new RegExp(/^\S+@\S+\.\S+$/);
+    let match = regex.test(String(this.emailForm.value.email));
+    if(this.emailForm.value.name!=""&&this.emailForm.value.text!=""&&match)
+    {
+      this.sendEmail(String(this.emailForm.value.name),String(this.emailForm.value.email),String(this.emailForm.value.text));
+    }
+    else {
+      if(this.emailForm.value.name=="")
+      this.nameError="Please enter a name";
+      if(!match)
+      this.emailError="Please enter a valid email";
+      if(this.emailForm.value.text=="")
+      this.textError="Please enter a message"
+    }
+  }
+
+  public sendEmail(name:string,email:string,text:string) {
     emailjs.init('ru0StxIRgzPLtQmCw')
     emailjs
       .send('service_k3wfiqz', 'template_1jjdovf',{
-        from_name:"men",
-        to_name:"tedo",
-        from_email:"testemail@gmail.com",
+        from_name:name,
+        to_name:"Olympics",
+        from_email:email,
         subject:'test',
-        message:'pls poluchi go'
+        message:text
       })
       .then(
         (response) => {
--- OlympicsWebsite/src/app/landing-page/landing-page.component.ts
+++ OlympicsWebsite/src/app/landing-page/landing-page.component.ts
@@ -20,6 +20,7 @@ export class LandingPageComponent {
   @ViewChild(NavigationComponent) nav?: NavigationComponent;
   @ViewChild(CarouselComponent) car?:CarouselComponent;
   photos:Image[]=[];
+  endDate=new Date('April 20, 2024 12:00:00').getTime();
   getPhotos(){
     this.photos=[{src:"https://scontent.fsof11-1.fna.fbcdn.net/v/t39.30808-6/407350386_757418879749858_5087436530491221890_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=3635dc&_nc_ohc=fZtR02CcY4sAX8RROqy&_nc_ht=scontent.fsof11-1.fna&oh=00_AfCufkHkd9js_GkUR2D_XcGfgoHFvs21SJLMQFRAVoJxbg&oe=65BC61BE", description:''},{src:'https://scontent.fsof11-1.fna.fbcdn.net/v/t39.30808-6/406263529_757418919749854_4016185538037317152_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=3635dc&_nc_ohc=UG8qCTpnaOkAX-MtIE-&_nc_ht=scontent.fsof11-1.fna&oh=00_AfCnyhIbbyMAhkM-FXMyEeoB6dVF19dXDBoeHQ2sIYVNbw&oe=65BBF9AA',description:''},{src:'https://scontent.fsof11-1.fna.fbcdn.net/v/t39.30808-6/406261675_757419016416511_1611717665302417932_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=3635dc&_nc_ohc=y3kP-3zdSnAAX80DDQs&_nc_ht=scontent.fsof11-1.fna&oh=00_AfC04GPylJhHA3uLXWxjPVpRjPjGdL0Os9VgrqBvLnEBTQ&oe=65BC251B',description:''}];
   }
@@ -33,29 +34,26 @@ export class LandingPageComponent {
     }, 1000);
   }
   getDays(){
-    let endDate=new Date('April 20, 2024 00:00:00').getTime();
     let now=new Date().getTime();
-    let remaining=endDate-now;
+    let remaining=this.endDate-now;
     return String(Math.floor(remaining/ (1000 * 3600 * 24))).padStart(2,"0");
   }
   getHours(){
     let endDate=new Date('April 20, 2024 00:00:00').getTime();
     let now=new Date().getTime();
-    let remaining=endDate-now;
+    let remaining=this.endDate-now;
     return String(Math.floor(remaining% (1000 * 3600 * 24)/(1000 * 3600 ))).padStart(2,"0");
   }
 
   getMinutes(){
-    let endDate=new Date('April 20, 2024 00:00:00').getTime();
     let now=new Date().getTime();
-    let remaining=endDate-now;
+    let remaining=this.endDate-now;
     return String(Math.floor(remaining% (1000 * 3600)/(1000 * 60 ))).padStart(2,"0");
   }
   
   getSeconds(){
-    let endDate=new Date('April 20, 2024 00:00:00').getTime();
     let now=new Date().getTime();
-    let remaining=endDate-now;
+    let remaining=this.endDate-now;
     return String(Math.floor(remaining% (1000 * 60)/(1000))).padStart(2,"0");
   }
 }
