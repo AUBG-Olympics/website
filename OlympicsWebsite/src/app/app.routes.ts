44ca2d8729a708637b4d0316c3f587f5313955c6
g-slavov <georgisslavov@gmail.com>
2024-03-02 19:42:53 +0200

    feat: introduce dday schedule page and add responsiveness

--- OlympicsWebsite/src/app/app.routes.ts
+++ OlympicsWebsite/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { WhoAreWePageComponent } from './who-are-we-page/who-are-we-page.compone
 import { SportWidgetComponent } from './components/sport-widget/sport-widget.component';
 import { SportsPageComponent } from './sports-page/sports-page.component';
 import { ContactUsPageComponent } from './contact-us-page/contact-us-page.component';
+import { SchedulePageComponent } from './schedule-page/schedule-page.component';
 
 export const routes: Routes = [
     {path:'',component:LandingPageComponent},
@@ -13,5 +14,6 @@ export const routes: Routes = [
     {path:'events/:event',component:EventsPageComponent},
     {path:'who-are-we',component:WhoAreWePageComponent},
     {path: 'dday/sports', component: SportsPageComponent},
+    {path: 'dday/schedule', component: SchedulePageComponent},
     {path:'contact-us',component:ContactUsPageComponent}
 ];
