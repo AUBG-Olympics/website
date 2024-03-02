2f2b891a1932a5063dfbe66e33068daa568a4d4b
Teodor <99684754+Pastedko@users.noreply.github.com>
2024-03-02 16:29:20 +0200

    ContactUsPage +  responsive navigation + responsive EventsPage (#24)
    * feat: contact us, responsive navigation and events

--- OlympicsWebsite/src/app/carousel/carousel.component.ts
+++ OlympicsWebsite/src/app/carousel/carousel.component.ts
@@ -31,6 +31,7 @@ export class CarouselComponent {
   constructor(private ngZone: NgZone){}
 
   ngAfterViewInit() {
+    console.log(this.numOfslides)
     this.ngZone.onStable
       .asObservable()
       .pipe(take(1))
@@ -40,12 +41,30 @@ export class CarouselComponent {
             perView: this.numOfslides,
             spacing:this.spacing,
             origin:'center'
+          },breakpoints:{
+            '(max-width:1200px)':{
+              slides:{
+                perView:this.numOfslides==1?1:3,            
+                spacing:this.spacing,
+                origin:'center'},
+            },'(max-width:900px)':{
+              slides:{
+                perView:this.numOfslides==1?1:2,            
+                spacing:this.spacing,
+                origin:'center'}
+            },'(max-width:600px)':{
+              slides:{
+                perView:1,     
+                spacing:this.spacing,
+                origin:'center'}
+            }
           },
           range:{
             align:true
           },
           mode:'free-snap',
           loop:true,
+          drag:this.arrows,
           renderMode:'performance',
           initial: this.currentSlide,
             slideChanged: (s) => {
