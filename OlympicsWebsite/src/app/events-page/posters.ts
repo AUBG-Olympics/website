43c7edc237076c1d18100785e099154a4fbb94dc
Teodor Kovachev <tedox.k@gmail.com>
2024-03-04 16:04:54 +0200

    image hosting updated

---
+++ OlympicsWebsite/src/app/events-page/posters.ts
@@ -0,0 +1,31 @@
+import { Image } from '../models/image'
+
+export const postersFall23: Image[] = [
+    {
+        src: "https://res.cloudinary.com/dq9gemegi/image/upload/v1709552789/Posters/6_dcrtyu.png",
+        description: "Volleyball"
+    },
+    {
+        src: "https://res.cloudinary.com/dq9gemegi/image/upload/v1709552798/Posters/5_pfmw04.png",
+        description: "Table tennis"
+    },
+    {
+        src: "https://res.cloudinary.com/dq9gemegi/image/upload/v1709552789/Posters/4_xcbyqw.png",
+        description: "Skills challenge"
+    }
+]
+
+export const postersSpring24: Image[]=[
+    {
+        src: "https://res.cloudinary.com/dq9gemegi/image/upload/v1709552784/Posters/3_elgpd8.png",
+        description: "Basketball"
+    },
+    {
+        src: "https://res.cloudinary.com/dq9gemegi/image/upload/v1709552797/Posters/2_iniumx.png",
+        description: "Dodgeball"
+    },
+    {
+        src: "https://res.cloudinary.com/dq9gemegi/image/upload/v1709552797/Posters/1_hzgao3.png",
+        description: "Spikeball"
+    }
+]
--- OlympicsWebsite/src/app/landing-page/landing-page.component.ts
+++ OlympicsWebsite/src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild,ViewEncapsulation } from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation, HostListener } from '@angular/core';
 import { NavigationComponent } from '../navigation/navigation.component';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { ThemeService } from '../services/theme.service';
@@ -8,77 +8,119 @@ import { Image } from '../models/image';
   selector: 'app-landing-page',
   standalone: true,
   providers: [ThemeService],
-  imports: [NavigationComponent,CarouselComponent],
+  imports: [NavigationComponent, CarouselComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 
 export class LandingPageComponent {
   @ViewChild(NavigationComponent) nav?: NavigationComponent;
-  @ViewChild(CarouselComponent) car?:CarouselComponent;
-  photos:Image[]=[];
-  endDate=new Date('April 21, 2024 09:00:00').getTime();
-
-  constructor(private themeService: ThemeService){}
-  getPhotos(){
-    this.photos=[
-      {
-      src:"https://res.cloudinary.com/dq9gemegi/image/upload/v1709540360/CarouselPhotos/DSC_0324_1_pmigoc.png",
-       description:''
-    },
-      {
-        src:'https://res.cloudinary.com/dq9gemegi/image/upload/v1709540359/CarouselPhotos/DSC_0324_7_qxxhez.png',
-        description:''
-      },
-      {
-        src:'https://res.cloudinary.com/dq9gemegi/image/upload/v1709540358/CarouselPhotos/DSC_0324_6_jbewrl.png',
-        description:''
-      },
-      {
-        src:'https://res.cloudinary.com/dq9gemegi/image/upload/v1709540358/CarouselPhotos/DSC_0324_5_gdzqg6.png',
-        description:''
-      }
-      ];
+  @ViewChild(CarouselComponent) car?: CarouselComponent;
+  photos: Image[] = [];
+  endDate = new Date('April 21, 2024 09:00:00').getTime();
+  public innerWidth: any;
+  desktop:boolean=true;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.innerWidth = window.innerWidth;
+    if(this.innerWidth>900){
+      if(!this.desktop){this.desktop=!this.desktop; this.getPhotos();}
+    }else{
+      if(this.desktop){this.desktop=!this.desktop;this.getPhotos();}
+    }
   }
-  ngOnInit(){
+
+  constructor(private themeService: ThemeService) { }
+  getPhotos() {
+    if(this.innerWidth>900){
+    this.photos = [
+      {
+        src: "https://res.cloudinary.com/dq9gemegi/image/upload/v1709549488/CarouselPhotos/425254669_810443281114084_2978768452918607329_n_u264qs.jpg",
+        description: ''
+      },
+      {
+        src: 'https://res.cloudinary.com/dq9gemegi/image/upload/v1709549486/CarouselPhotos/428502175_810441871114225_4628992403078249897_n_gdnfw4.jpg',
+        description: ''
+      },
+      {
+        src: 'https://res.cloudinary.com/dq9gemegi/image/upload/v1709549485/CarouselPhotos/428491293_813861317438947_2298361993371666072_n_plgzly.jpg',
+        description: ''
+      },
+      {
+        src: 'https://res.cloudinary.com/dq9gemegi/image/upload/v1709549483/CarouselPhotos/428508360_813861040772308_3018515262349797840_n_zy7lto.jpg',
+        description: ''
+      },
+      {
+        src: 'https://res.cloudinary.com/dq9gemegi/image/upload/v1709549483/CarouselPhotos/431059903_813858507439228_6567449649785649249_n_aqmv9j.jpg',
+        description: ''
+      },
+      {
+        src: 'https://res.cloudinary.com/dq9gemegi/image/upload/v1709549478/CarouselPhotos/424972233_810440724447673_9217132925745596085_n_k7zae2.jpg',
+        description: ''
+      }
+    ];}
+    else{
+      this.photos = [
+        {
+          src: "https://res.cloudinary.com/dq9gemegi/image/upload/v1709550920/CarouselPhotos/image3_azvijg.png",
+          description: ''
+        },
+        {
+          src: 'https://res.cloudinary.com/dq9gemegi/image/upload/v1709550288/CarouselPhotos/425330340_810440677781011_8222974675471495997_n_ynkadr.jpg',
+          description: ''
+        },
+        {
+          src: 'https://res.cloudinary.com/dq9gemegi/image/upload/v1709550291/CarouselPhotos/image_u5ytqp.png',
+          description: ''
+        },
+        {
+          src: 'https://res.cloudinary.com/dq9gemegi/image/upload/v1709550741/CarouselPhotos/image2_n4wm57.png',
+          description: ''
+        }
+      ]
+    }
+    console.log(this.photos)
+  }
+  ngOnInit() {
+    this.innerWidth = window.innerWidth;
     this.getPhotos();
     const themePreference = sessionStorage.getItem('theme');
 
-    if(themePreference === 'dark'){
+    if (themePreference === 'dark') {
       this.themeService.setDarkTheme();
-    } else if(themePreference === 'light'){
+    } else if (themePreference === 'light') {
       this.themeService.setLightTheme();
-    } else if(themePreference === 'dday'){
+    } else if (themePreference === 'dday') {
       this.themeService.setDDayTheme();
     }
   }
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     setInterval(() => {
-      this.getSeconds() 
+      this.getSeconds()
     }, 1000);
   }
-  getDays(){
-    let now=new Date().getTime();
-    let remaining=this.endDate-now;
-    return String(Math.floor(remaining/ (1000 * 3600 * 24))).padStart(2,"0");
+  getDays() {
+    let now = new Date().getTime();
+    let remaining = this.endDate - now;
+    return String(Math.floor(remaining / (1000 * 3600 * 24))).padStart(2, "0");
   }
-  getHours(){
-    let endDate=new Date('April 20, 2024 00:00:00').getTime();
-    let now=new Date().getTime();
-    let remaining=this.endDate-now;
-    return String(Math.floor(remaining% (1000 * 3600 * 24)/(1000 * 3600 ))).padStart(2,"0");
+  getHours() {
+    let endDate = new Date('April 20, 2024 00:00:00').getTime();
+    let now = new Date().getTime();
+    let remaining = this.endDate - now;
+    return String(Math.floor(remaining % (1000 * 3600 * 24) / (1000 * 3600))).padStart(2, "0");
   }
 
-  getMinutes(){
-    let now=new Date().getTime();
-    let remaining=this.endDate-now;
-    return String(Math.floor(remaining% (1000 * 3600)/(1000 * 60 ))).padStart(2,"0");
+  getMinutes() {
+    let now = new Date().getTime();
+    let remaining = this.endDate - now;
+    return String(Math.floor(remaining % (1000 * 3600) / (1000 * 60))).padStart(2, "0");
   }
-  
-  getSeconds(){
-    let now=new Date().getTime();
-    let remaining=this.endDate-now;
-    return String(Math.floor(remaining% (1000 * 60)/(1000))).padStart(2,"0");
+
+  getSeconds() {
+    let now = new Date().getTime();
+    let remaining = this.endDate - now;
+    return String(Math.floor(remaining % (1000 * 60) / (1000))).padStart(2, "0");
   }
 }
