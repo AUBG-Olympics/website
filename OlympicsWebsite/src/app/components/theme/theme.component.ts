2f18a4835e0090a56ebbd70156e679782cfda872
AUBG Olympics <147837706+aubgolympics@users.noreply.github.com>
2024-03-04 23:52:30 +0200

    chore: final bug fixing and feat introduction (#30)

--- OlympicsWebsite/src/app/components/theme/theme.component.ts
+++ OlympicsWebsite/src/app/components/theme/theme.component.ts
@@ -12,11 +12,29 @@ import { CommonModule } from '@angular/common';
 })
 export class ThemeComponent {
 
-  constructor(private themeService: ThemeService){}
-  public onClick(): void{
-    
-    this.themeService.setDarkTheme();
+  constructor(private themeService: ThemeService){
+    const selectedTheme = sessionStorage.getItem('theme');
+
+    if(selectedTheme){
+      if(selectedTheme === "light"){
+        this.selectedTraditional = true;
+        this.selectedReversed = false;
+        this.selectedDDay = false;
+      } else if(selectedTheme === 'dark'){
+        this.selectedTraditional = false;
+        this.selectedReversed = true;
+        this.selectedDDay = false;
+      } else if(selectedTheme === 'dday'){
+        this.selectedTraditional = false;
+        this.selectedReversed = false;
+        this.selectedDDay = true;
+      }
+    }
   }
+
+  public selectedTraditional: boolean = true;
+  public selectedReversed: boolean = false;
+  public selectedDDay: boolean = false;
 
   isDropdownOpen = false;
 
@@ -24,8 +42,21 @@ export class ThemeComponent {
     this.isDropdownOpen = !this.isDropdownOpen;
   }
 
-  public selectedTheme(selectedTheme: string){
+  public selectedTheme(element: any, selectedTheme: string){
     this.isDropdownOpen = false;
+    this.selectedTraditional = false;
+    this.selectedReversed = false;
+    this.selectedDDay = false;
+
+    let target = element.target.textContent;
+
+    if(target === "Traditional"){
+      this.selectedTraditional = true;
+    } else if(target === "Reversed"){
+      this.selectedReversed = true;
+    } else if(target === "D-Day"){
+      this.selectedDDay = true;
+    }
 
     if(selectedTheme === 'light'){
       this.themeService.setLightTheme();
--- OlympicsWebsite/src/app/schedule-page/schedule-page.component.ts
+++ OlympicsWebsite/src/app/schedule-page/schedule-page.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
 import { NavigationComponent } from '../navigation/navigation.component';
+import { ThemeService } from '../services/theme.service';
 
 @Component({
   selector: 'app-schedule-page',
@@ -11,5 +12,16 @@ import { NavigationComponent } from '../navigation/navigation.component';
   encapsulation: ViewEncapsulation.None
 })
 export class SchedulePageComponent {
+  constructor(private themeService: ThemeService){}
+  ngOnInit(){
+    const themePreference = sessionStorage.getItem('theme');
 
+    if(themePreference === 'dark'){
+      this.themeService.setDarkTheme();
+    } else if(themePreference === 'light'){
+      this.themeService.setLightTheme();
+    } else if(themePreference === 'dday'){
+      this.themeService.setDDayTheme();
+    }
+  }
 }
