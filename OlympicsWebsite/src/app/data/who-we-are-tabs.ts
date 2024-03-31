648436cfdfbfe20f70502e05b90f2e84a18885f9
g-slavov <georgisslavov@gmail.com>
2024-03-31 02:28:30 +0200

    feat: initiate who we are page

---
+++ OlympicsWebsite/src/app/data/who-we-are-tabs.ts
@@ -0,0 +1,35 @@
+export const whoWeAreTabsEn = [
+    {
+        ImageUrl: 'https://res.cloudinary.com/dq9gemegi/image/upload/v1711839024/WhoWeAre/AlumniCrew.jpg',
+        Content: 'AUBG Olympics is the biggest sports club at the American University in Bulgaria. Our crew organizes many sports challenges and workshops to promote an active lifestyle among young people.',
+        Position: 'left'
+    },
+    {
+        ImageUrl: 'https://res.cloudinary.com/dq9gemegi/image/upload/v1711838667/WhoWeAre/OpeningCeremony.jpg',
+        Content: 'We organize the largest student sports Olympiad in the country, where current and former students, professors, staff, and local residents compete in 18 different sports throughout the weekend.',
+        Position: 'right'
+    }, 
+    {
+        ImageUrl: 'https://res.cloudinary.com/dq9gemegi/image/upload/v1711843010/WhoWeAre/FireCeremony.png',
+        Content: 'To mark the start of the Olympiad, we are also organizing a spectacular opening ceremony where we light the Olympic fire just like the Greek gods of Olympus.',
+        Position: 'left'
+    }
+]
+
+export const whoWeAreTabsBg = [
+    {
+        ImageUrl: 'https://res.cloudinary.com/dq9gemegi/image/upload/v1711839024/WhoWeAre/AlumniCrew.jpg',
+        Content: 'АУБ Олимпикс е най-големият спортен клуб в Американския университет в България. Екипът ни организира редица спортни състезания и семинари, за да популяризира активния начин на живот сред младите, включително най-голямата студентска спортна олимпиада в страната.',
+        Position: 'left'
+    },
+    {
+        ImageUrl: 'https://res.cloudinary.com/dq9gemegi/image/upload/v1711838667/WhoWeAre/OpeningCeremony.jpg',
+        Content: 'Участниците - настоящи и бивши студенти, професори, персонал и местни жители, се състезават в 18 различни спортни дисциплини в рамките на един уикенд. 211 от тях  стават медалисти и биват наградени от специален гост - виден български спортист. ',
+        Position: 'right'
+    }, 
+    {
+        ImageUrl: 'https://res.cloudinary.com/dq9gemegi/image/upload/v1711843010/WhoWeAre/FireCeremony.png',
+        Content: 'За да отбележим началото на олимпиадата, организираме и зрелищна церемония по откриването, на която запалваме олимпийския огън по подобие на гръцките богове на Олимп.',
+        Position: 'left'
+    }
+]
