127fbc8c8f443dc751cd57a67bbbaf8c54890077
Teodor <99684754+Pastedko@users.noreply.github.com>
2024-04-02 20:55:44 +0300

    37 events page (#52)
    * fix: olympics logo size and countdown on mobile and easter egg 
    ---------
    
    Co-authored-by: g-slavov <georgisslavov@gmail.com>

--- OlympicsWebsite/src/app/events-page/posters.ts
+++ OlympicsWebsite/src/app/events-page/posters.ts
@@ -1,31 +1,45 @@
-import { Image } from '../models/image'
+import { Event } from '../models/event'
 
-export const postersFall23: Image[] = [
+export const postersFall23: Event[] = [
     {
         src: "https://res.cloudinary.com/dq9gemegi/image/upload/v1709552789/Posters/6_dcrtyu.png",
-        description: "Volleyball"
+        description: "Volleyball",
+        url:"https://www.instagram.com/reel/Czt8eZmofmq/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
     },
     {
         src: "https://res.cloudinary.com/dq9gemegi/image/upload/v1709552798/Posters/5_pfmw04.png",
-        description: "Table tennis"
+        description: "Table tennis",
+        url:"https://www.instagram.com/reel/Cz_10NAIcE5/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
     },
     {
         src: "https://res.cloudinary.com/dq9gemegi/image/upload/v1709552789/Posters/4_xcbyqw.png",
-        description: "Skills challenge"
+        description: "Skills challenge",
+        url:"https://youtube.com/shorts/svsyrXUNfUo?si=sMyiYV7E9xNBs7eS"
     }
 ]
 
-export const postersSpring24: Image[]=[
+export const postersSpring24: Event[]=[
     {
         src: "https://res.cloudinary.com/dq9gemegi/image/upload/v1709552784/Posters/3_elgpd8.png",
-        description: "Basketball"
+        description: "Basketball",
+        url:"https://www.instagram.com/reel/C3sGdteIEWa/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
     },
     {
-        src: "https://res.cloudinary.com/dq9gemegi/image/upload/v1709552797/Posters/2_iniumx.png",
-        description: "Dodgeball"
+        src: "https://res.cloudinary.com/dq9gemegi/image/upload/v1711995382/Posters/5_ghq9ks.png",
+        description: "Dodgeball",
+        url:"https://www.instagram.com/reel/C3-o461IPc1/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
     },
     {
-        src: "https://res.cloudinary.com/dq9gemegi/image/upload/v1709552797/Posters/1_hzgao3.png",
-        description: "Spikeball"
+        src: "https://res.cloudinary.com/dq9gemegi/image/upload/v1711995383/Posters/4_tsqih8.png",
+        description: "Spikeball",
+        url:"https://www.instagram.com/reel/C4Qq-1lI_kv/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+    }
+]
+
+export const postersDDay24: Event[]=[
+    {
+        src:"https://res.cloudinary.com/dq9gemegi/image/upload/v1711998073/Posters/20_days_till_dday_d4de9b.png",
+        description:"D-DAY-21.04",
+        url:"https://www.instagram.com/reel/C5OoJECoaDZ/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
     }
 ]
